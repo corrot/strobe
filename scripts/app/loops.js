a161780315fbcd0d3a8e2bf88955f7922baf2be4
corrot <gkoroshinadze@gmail.com>
2018-03-23 21:08:22 +0400

    removed node_modules and package.json, changes added

--- scripts/app/loops.js
+++ scripts/app/loops.js
@@ -1,8 +1,6 @@
-var loops = [
-    {
+var loops = [{
         barCount: 16,
-        data:[
-            {
+        data: [{
                 start: 0,
                 end: 1
             },
@@ -14,8 +12,7 @@ var loops = [
     },
     {
         barCount: 16,
-        data:[
-            {
+        data: [{
                 start: 1,
                 end: 2
             },
@@ -27,8 +24,7 @@ var loops = [
     },
     {
         barCount: 16,
-        data:[
-            {
+        data: [{
                 start: 0,
                 end: 1
             },
@@ -44,30 +40,46 @@ var loops = [
     },
 ];
 
-
 strobeApp.controller('loopsController', ['$scope', function($scope) {
     $scope.loops = loops;
-    
+
     $scope.selectedAction = {};
-    $scope.selectAction = function(o, i){
-        $scope.selectedAction = o;
-        $scope.selectedAction.loopIndex = i;
+    $scope.selectAction = function(o, i) {
+        if ($scope.selectedAction == o && $scope.selectedAction.loopIndex == i) {
+            $scope.selectedAction = {};
+        } else {
+            $scope.selectedAction = o;
+            $scope.selectedAction.loopIndex = i;
+        }
+        $scope.selectedBar = {};
     }
 
     $scope.selectedBar = {};
-    $scope.selectBar = function(i, j){
-        $scope.selectedBar = {
-            loopIndex: i,
-            barIndex: j
+    $scope.selectBar = function(i, j) {
+        if ($scope.selectedBar.loopIndex == i && $scope.selectedBar.barIndex == j) {
+            $scope.selectedBar = {};
+        } else {
+            $scope.selectedBar = {
+                loopIndex: i,
+                barIndex: j
+            }
         }
+        $scope.selectedAction = {};
     }
 
-    $scope.addAction = function(){
-        $scope.loops[$scope.selectedBar.loopIndex].data.push({start: $scope.selectedBar.barIndex, end: $scope.selectedBar.barIndex+1})
+    $scope.addAction = function() {
+        var o = { start: $scope.selectedBar.barIndex, end: $scope.selectedBar.barIndex + 1 };
+        $scope.loops[$scope.selectedBar.loopIndex].data.push(o);
+        $scope.selectAction(o, $scope.selectedBar.loopIndex);
     }
 
-    $scope.removeAction = function(){
+    $scope.removeAction = function() {
         var i = $scope.loops[$scope.selectedAction.loopIndex].data.indexOf($scope.selectedAction);
         $scope.loops[$scope.selectedAction.loopIndex].data.splice(i, 1);
+        $scope.selectedAction = {};
+    }
+
+    $scope.test = function() {
+        console.log('resized!');
     }
 }]);
