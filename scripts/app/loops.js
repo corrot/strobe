325fdd7f2d81eb41b4e35798cb8a78cbd5ae957b
corrot <gkoroshinadze@gmail.com>
2018-03-24 01:43:55 +0400

    marks added, delete with rightclick

--- scripts/app/loops.js
+++ scripts/app/loops.js
@@ -1,18 +1,18 @@
 var barCount = 106;
-var loops = 
-function(){
-    var result = [];
-    for(var i = 0; i < 16; i++){
-        result.push([]);
-    }
-    return result;
-}();
+var loops =
+    function() {
+        var result = [];
+        for (var i = 0; i < 16; i++) {
+            result.push([]);
+        }
+        return result;
+    }();
 
 strobeApp.controller('loopsController', ['$scope', function($scope) {
     $scope.loops = loops;
     $scope.barCount = barCount;
     $scope.selectedAction = {};
-   
+
     $scope.selectAction = function(o, i) {
         if ($scope.selectedAction == o && $scope.selectedAction.loopIndex == i) {
             $scope.selectedAction = {};
@@ -27,44 +27,41 @@ strobeApp.controller('loopsController', ['$scope', function($scope) {
         var loop = $scope.loops[loopIndex];
         var cell = { start: start, end: end };
         var isRedundant;
-        loop.forEach((c)=>{
-            if(c.start <= cell.start && c.end >= cell.end){
+        loop.forEach((c) => {
+            if (c.start <= cell.start && c.end >= cell.end) {
                 isRedundant = true;
                 return;
             }
         });
 
-        if(isRedundant){
+        if (isRedundant) {
             return;
         }
 
-        var cellsToMerge = $.grep(loop, (o)=>{
-                if(o.end == cell.start || o.start == cell.end){
-                    return true;
-                }
+        var cellsToMerge = $.grep(loop, (o) => {
+            if (o.end == cell.start || o.start == cell.end) {
+                return true;
+            }
         });
 
-        if(cellsToMerge && cellsToMerge.length > 0){
-           cellsToMerge.forEach(c => {
-               if(c.end == cell.start){
-                   cell.start = c.start
-               }
-               else{
-                   cell.end = c.end;
-               }
+        if (cellsToMerge && cellsToMerge.length > 0) {
+            cellsToMerge.forEach(c => {
+                if (c.end == cell.start) {
+                    cell.start = c.start
+                } else {
+                    cell.end = c.end;
+                }
 
-               loop.splice(loop.indexOf(c), 1)
-           });
-        }     
+                loop.splice(loop.indexOf(c), 1)
+            });
+        }
 
         loop.push(cell);
         $scope.selectAction(cell, loop.indexOf(cell));
     }
 
-    $scope.removeAction = function() {
-        var i = $scope.loops[$scope.selectedAction.loopIndex].indexOf($scope.selectedAction);
-        $scope.loops[$scope.selectedAction.loopIndex].splice(i, 1);
-        $scope.selectedAction = {};
+    $scope.removeAction = function(o, i, j) {
+        $scope.loops[i].splice(j, 1);
     }
 
     $scope.test = function() {
@@ -79,27 +76,37 @@ strobeApp.controller('loopsController', ['$scope', function($scope) {
     //     }
     // });
 
-    document.body.onmousedown = function() { 
+    document.body.onmousedown = function() {
         $scope.isMouseClicked = true;
-      }
+    }
 
-      document.body.onmouseup = function() {
+    document.body.onmouseup = function() {
         $scope.isMouseClicked = false;
-      }
+    }
 }]);
-
-document.addEventListener('keydown', (event) => {
-    if(event.key == 'Control')
-        ctrl = true;
-
-    if(event.key == 'Shift')
-        shift = true;
+strobeApp.directive('ngRightClick', function($parse) {
+    return function(scope, element, attrs) {
+        var fn = $parse(attrs.ngRightClick);
+        element.bind('contextmenu', function(event) {
+            scope.$apply(function() {
+                event.preventDefault();
+                fn(scope, { $event: event });
+            });
+        });
+    };
 });
+// document.addEventListener('keydown', (event) => {
+//     if (event.key == 'Control')
+//         ctrl = true;
 
-document.addEventListener('keydown', (event) => {
-    if(event.key == 'Control')
-        ctrl = false;
+//     if (event.key == 'Shift')
+//         shift = true;
+// });
 
-    if(event.key == 'Shift')
-        shift = false;
-});
+// document.addEventListener('keydown', (event) => {
+//     if (event.key == 'Control')
+//         ctrl = false;
+
+//     if (event.key == 'Shift')
+//         shift = false;
+// });
